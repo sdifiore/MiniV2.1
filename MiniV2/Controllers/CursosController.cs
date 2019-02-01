2806b6b404ae90d11695aeda612f4091c8a0954d
Sergio Di Fiore <difiores@outlook.com>
2019-02-01 15:13:23 -0200

    Galeria de cursos

---
+++ MiniV2/Controllers/CursosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniV2.Controllers
+{
+    public class CursosController : Controller
+    {
+        // GET: Cursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Receitas_rapidas_para_detox_30_jan_2018()
+        {
+            return View();
+        }
+    }
+}
