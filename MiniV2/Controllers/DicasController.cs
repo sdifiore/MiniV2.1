7ff9dc113dfef0e770a38bbbbe54e02700b78886
Sergio Di Fiore <difiores@outlook.com>
2019-03-04 17:52:50 -0300

    O que manter na despensa e na geladeira

--- MiniV2/Controllers/DicasController.cs
+++ MiniV2/Controllers/DicasController.cs
@@ -18,5 +18,12 @@ namespace MiniV2.Controllers
 
             return View();
         }
+
+        public ActionResult O_que_manter_na_despensa_e_na_geladeira()
+        {
+            ViewBag.Title = "O que manter na despensa e na geladeira";
+
+            return View();
+        }
     }
 }
