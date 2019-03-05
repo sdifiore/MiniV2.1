e400d301b6d9d2ed19c0016786f27c53f612f8c5
Sergio Di Fiore <difiores@outlook.com>
2019-03-05 05:42:04 -0300

    Receitas e intestino saudável 22 de fevereiro de 2019

--- MiniV2/Controllers/CursosController.cs
+++ MiniV2/Controllers/CursosController.cs
@@ -10,8 +10,17 @@ namespace MiniV2.Controllers
             return View();
         }
 
-        public ActionResult Receitas_rapidas_para_detox_30_jan_2018()
+        public ActionResult Receitas_rapidas_para_detox_30_jan_2019()
         {
+            ViewBag.Title = "Receitas rápidas para detox 30-jan-2019";
+
+            return View();
+        }
+
+        public ActionResult Receitas_e_intestino_saudavel_22_fev_2019()
+        {
+            ViewBag.Title = "Receitas e intestino saudável 22 de fevereiro de 2019";
+
             return View();
         }
     }
