5eb6402a4fc3e6000a7b2b35725404cb61ac400a
Sergio Di Fiore <difiores@outlook.com>
2019-08-24 17:58:14 -0300

    fodmaps em tabs

--- MiniV2/Controllers/DicasController.cs
+++ MiniV2/Controllers/DicasController.cs
@@ -26,6 +26,62 @@ namespace MiniV2.Controllers
             return View();
         }
 
+        public ActionResult Vegetais()
+        {
+            ViewBag.Title = "Vegetais ricos em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Frutas()
+        {
+            ViewBag.Title = "Frutas ricas em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Leite_e_Derivados()
+        {
+            ViewBag.Title = "Leite e derivados ricos em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Substitutos_do_leite()
+        {
+            ViewBag.Title = "Substitutos de leite e derivados ricos em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Leguminosas_oleaginosas_graos_sementes()
+        {
+            ViewBag.Title = "Leguminosas, oleaginosas, gr&atilde;os e sementes ricas em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Bebidas()
+        {
+            ViewBag.Title = "Bebidas ricas em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Condimentos_e_acucares()
+        {
+            ViewBag.Title = "Condimentos e a&ccedil;&uacute;cares ricos em FODMAPS";
+
+            return View();
+        }
+
+        public ActionResult Industrializados()
+        {
+            ViewBag.Title = "Aten&ccedil;&atilde;o nos ingredientes de produtos industrializados!!";
+
+            return View();
+        }
+
         public ActionResult O_que_manter_na_despensa_e_na_geladeira()
         {
             ViewBag.Title = "O que manter na despensa e na geladeira";
